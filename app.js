1a90204142f9d84a15a46cbe77db4e11a1299806
Evgenia-N <jazzpenguin@yandex.ru>
2022-03-28 23:01:03 +0300

    feat: mongoose, models

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Все в порядке. Приложение слушает порт ${PORT}`);
-});
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('Установлена связь с базой данных');
+
+  app.listen(PORT, () => {
+    console.log(`Все в порядке. Приложение слушает порт ${PORT}`);
+  });
+}
+
+main();
