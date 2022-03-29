462fcad263746bd6f9fa0136f56277fd088ef604
Evgenia-N <jazzpenguin@yandex.ru>
2022-03-29 23:11:41 +0300

    fix: routes/users

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const userRoutes = express.Router();
 
-// const auth = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 // const {
 //  validateUserInfo, validateLogin, validateRegister, validateLogout,
 // } = require('../middlewares/validation');
 
-const { getThisUser, updateUser } = require('../controllers/users');
+const {
+  getThisUser, updateUser, login, createUser, logout,
+} = require('../controllers/users');
 
 userRoutes.get('/users/me', auth, getThisUser);
 userRoutes.patch('/users/me', auth, express.json(), validateUserInfo, updateUser);
